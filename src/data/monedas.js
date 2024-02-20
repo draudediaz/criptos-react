116ea84353825705bef53bebba83fe5ba81f2bd1
draudediaz <draude.diaz@gmail.com>
2024-02-20 22:11:18 +0100

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas=[
+    {id:"EUR", nombre: "Euro"},
+    {id:"USD", nombre: "Dòlar d'Estats Units"},
+    {id:"MXN", nombre: "Pes Mexicà"},
+    {id:"GBP", nombre: "Lliure Estarlina"}
+]
+
+export {
+    monedas
+}
